5bd29614ad894047c135285c55a1e7a6227d44ce
mbai97 <mbaibrian13@gmail.com>
2021-12-10 09:40:05 +0300

    first commit

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
+from user import views
+# from django.conf import settings
+#\ from django.conf.urls.static import static
+# from django.urls.resolvers import URLPattern
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
+    path('', include('user.urls')),
+    # path('', views.home, name='home'),
 ]
+
+# if settings.DEBUG:
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth import SESSION_KEY, authenticate, login
+from django.contrib.auth.models import User, auth
+from django.conf import settings
+from django.contrib import messages
+from django.utils import timezone
+from random import randint
 
-# Create your views here.
+def anonymous_required(function=None, redirect_url=None):
+    if not redirect_url:
+        redirect_url = 'dashboard'
+        
+        actual_decorator = user_passes_test(
+            lambda u: u.is_anonymous,
+            login_url=redirect_url
+        )
+        
+        if function:
+            return actual_decorator(function)
+        return actual_decorator
+    
+@login_required
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+
+@anonymous_required
+def login(request):
+    
+    return render (request, 'user/login.html', {})
+    
+    
+@anonymous_required
+def register(request):
+    
+    return render (request, 'user/register.html', {})
+
+@login_required
+def dashboard(request):
+    
+    return render (request, 'user/dashboard.html', {})
+    
+
+    
+    
